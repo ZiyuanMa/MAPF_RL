cdeecc69dd69d07235c5ee9db104bd92006b1ee9
Ziyuan Ma <ziyuanma000@gmail.com>
2020-11-03 11:24:05 -0800

    new test

--- test.py
+++ test.py
@@ -176,7 +176,7 @@ def test_model(map_length, density):
     state_dict = torch.load('./model.pth', map_location=device)
     network.load_state_dict(state_dict)
 
-    num_agents = 4
+    num_agents = 2
 
     while os.path.exists('./test{}_{}_{}.pkl'.format(num_agents, map_length, density)):
 
@@ -312,7 +312,7 @@ def make_animation():
 
 if __name__ == '__main__':
 
-    # create_test(4, 5, 0.3)
-    # test_model(5, 0.3)
-    make_animation()
+    # create_test(2, 20, 0.3)
+    test_model(20, 0.3)
+    # make_animation()
     # create_test(1, 20)
